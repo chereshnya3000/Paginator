5ca8b60e9025f7a6cd1adef2efc0fe9abe877e7c
chereshnya3000 <chereshnya3000@yandex.ru>
2015-11-04 23:53:49 +0200

    newPaginator

--- paginator.js
+++ paginator.js
@@ -19,81 +19,71 @@ $(document).ready(function() {
 
   pagination.append("<div class='pagi'></div>");
 
-  var resultsAmount = 1000000;
-  var maxOnPage = 10;
-  var pagesQty = resultsAmount/maxOnPage
+  var comingData = {
+    resultsAmount: 1000000,
+    maxOnPage: 10
+  };
 
- var comingData = [
+  var resultsAmount = comingData.resultsAmount;
+  var maxOnPage = comingData.maxOnPage;
+  var pagesQty = resultsAmount/maxOnPage;
 
-    {pageNumber: 1, url: "http://website.com/?p=1"}, 
-    {pageNumber: 2, url: "http://website.com/?p=2"},
-    {pageNumber: 3, url: "http://website.com/?p=3"}
+  var buttonClick = function(index, elem){
+    console.log(index);
+    console.log(elem);
+  }
 
-  ];
+  /* определяем переменную - количество ссылок отображаемых в pagination */
+  var linksQty = 10;
 
   var pagi = $(".pagi");
-  for (var i = 0; i < 3 ; i++){
-
+  
+  for (var i = 0; i < linksQty; i++) 
+  {
     var pageIndex = i+1;
-    pagi.append("<a href ='" + comingData[i].url + "'>" + comingData[i].pageNumber + "</a>");
+    pagi.append("<span>" + pageIndex + "</span>");
+
+    $(pagi.children().last()).click(
+
+        (function(x)
+          {return function(){
+            $(currentActElem).removeAttr("class");
+            $(this).addClass("active");
+            currentActElem = this;
+            buttonClick(x, currentActElem);
+            };
+            })(pageIndex));
 
   }
 
-  
 
-//   /* определяем переменную - количество ссылок отображаемых в pagination */
-//   var linksQty = 10;
-
-//   var pagi = $(".pagi");
-  
-//   for (var i = 0; i < linksQty; i++) 
-//   {
-//     var pageIndex = i+1;
-//     pagi.append("<span>" + pageIndex + "</span>");
-  
-//     $(pagi.children().last()).click(function() {
-//       $(currentActElem).removeAttr("class");
-//       $(this).addClass("active");
-//       currentActElem = this;
-//     });
-//   }
-
-//   var currentActElem = pagi.children()[0];
-//   $(currentActElem).addClass("active");  
+  var currentActElem = pagi.children()[0];
+  $(currentActElem).addClass("active");  
  
-//  $(".previous").click(function() {
-//     var prevElem = $(currentActElem).prev();
-//     var myFirst = $(currentActElem).parent().children().first();
-//     if (myFirst.is($(currentActElem))){
-//     return;
-//   }
-//     $(currentActElem).removeAttr("class");
-//     $(prevElem).addClass("active");
-//     currentActElem = prevElem;
-//  });
+ $(".previous").click(function() {
+    var prevElem = $(currentActElem).prev();
+    var myFirst = $(currentActElem).parent().children().first();
+    if (myFirst.is($(currentActElem))){
+    return;
+  }
+    $(currentActElem).removeAttr("class");
+    $(prevElem).addClass("active");
+    currentActElem = prevElem;
+ });
 
 
 
-// $(".next").click(function() {
-//     var nextElem = $(currentActElem).next();
-//     var myLast = $(currentActElem).parent().children().last();
-//     if (myLast.is($(currentActElem))){
-//     return;
-//   }
-//     $(currentActElem).removeAttr("class");
-//     var nextElem = $(currentActElem).next();
-//     $(nextElem).addClass("active");
-//     currentActElem = nextElem;
-//  });
-
-
-
-
-    
-
-
-
-
+$(".next").click(function() {
+    var nextElem = $(currentActElem).next();
+    var myLast = $(currentActElem).parent().children().last();
+    if (myLast.is($(currentActElem))){
+    return;
+  }
+    $(currentActElem).removeAttr("class");
+    var nextElem = $(currentActElem).next();
+    $(nextElem).addClass("active");
+    currentActElem = nextElem;
+ });
 
 
   
